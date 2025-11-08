670b5b46ccdf5ea776486997dcdf535dcc84c763
Gonzalo Farfan <demaniturron@gmail.com>
2025-11-08 17:31:03 -0300

    add comentario 2

--- gateway/src/main/java/backend/grupo130/gateway/GatewayApplication.java
+++ gateway/src/main/java/backend/grupo130/gateway/GatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
-
+    // tarda lo suyo
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
