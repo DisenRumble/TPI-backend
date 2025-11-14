ca3f32f808b65a98d834dbb6c49c214c0222e69b
DisenRumble <juanpabloquintana2005@gmail.com>
2025-11-14 20:48:14 -0300

    fix

---
+++ usuarios-svc/src/main/java/backend/grupo73/usuarios_svc/UsuariosSvcApplication.java
@@ -0,0 +1,13 @@
+package backend.grupo73.usuarios_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosSvcApplication.class, args);
+	}
+
+}
---
+++ usuarios-svc/src/test/java/backend/grupo73/usuarios_svc/UsuariosSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.grupo73.usuarios_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
