e4a76a6c33e02fde6e95294b88e628b3b70c54a3
Gonzalo Farfan <demaniturron@gmail.com>
2025-11-14 21:47:36 -0300

    add camiones-svc

---
+++ camiones-svc/src/main/java/backend/grupo73/camiones/CamionesSvcApplication.java
@@ -0,0 +1,13 @@
+package backend.grupo73.camiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamionesSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamionesSvcApplication.class, args);
+	}
+
+}
---
+++ camiones-svc/src/test/java/backend/grupo73/camiones/CamionesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.grupo73.camiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamionesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
