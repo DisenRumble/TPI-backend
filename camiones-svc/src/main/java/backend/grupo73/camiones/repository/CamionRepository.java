2c38029c735f9624ff192004000d1a51771b4d7b
Gonzalo Farfan <demaniturron@gmail.com>
2025-11-15 09:57:13 -0300

    remove camiones-svc
    estaba para la bosta y tuve q borrarlo, dps creamos otro

--- camiones-svc/src/main/java/backend/grupo73/camiones/repository/CamionRepository.java
+++
@@ -1,9 +0,0 @@
-package backend.grupo73.camiones.repository;
-
-import backend.grupo73.camiones.model.Camion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CamionRepository extends JpaRepository<Camion, Long> {
-}
